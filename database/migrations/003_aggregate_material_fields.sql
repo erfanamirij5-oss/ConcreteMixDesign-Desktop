1cb910fb6fd3cc3b8abd9ab8954508273e149092
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-04 02:48:07 +0330

    Add aggregate material fields migration

---
+++ database/migrations/003_aggregate_material_fields.sql
@@ -0,0 +1,4 @@
+ALTER TABLE materials ADD COLUMN aggregate_role TEXT;
+ALTER TABLE materials ADD COLUMN nominal_size_mm REAL;
+ALTER TABLE materials ADD COLUMN fractured_face_percent REAL;
+ALTER TABLE materials ADD COLUMN moisture_condition TEXT;
