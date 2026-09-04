1c8d73bed12e20c406c801ddcdc7d8b37e789791
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-04 12:27:48 +0330

    Add recycled water monitoring fields

---
+++ database/migrations/011_recycled_water_monitoring.sql
@@ -0,0 +1,5 @@
+ALTER TABLE materials ADD COLUMN water_source_class TEXT;
+ALTER TABLE materials ADD COLUMN c1602_last_qualification_date TEXT;
+ALTER TABLE materials ADD COLUMN c1602_last_density_check_date TEXT;
+ALTER TABLE materials ADD COLUMN c1602_density_monitoring_method TEXT;
+ALTER TABLE materials ADD COLUMN c1602_monitoring_evidence_ref TEXT;
