9d01674d458f45ec6f2b82cea4a575ce244062d0
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-04 22:12:36 +0330

    enforce trial completion evidence in database

--- database/migrations/020_minimum_trial_mix.sql
+++ database/migrations/020_minimum_trial_mix.sql
@@ -18,3 +18,20 @@ CREATE TABLE IF NOT EXISTS trial_mix_records (
 
 CREATE INDEX IF NOT EXISTS idx_trial_mix_records_mix_design
   ON trial_mix_records(mix_design_id, trial_date DESC, created_at DESC);
+
+CREATE TRIGGER IF NOT EXISTS trg_mix_design_trial_completion_requires_record
+BEFORE UPDATE OF status ON mix_designs
+WHEN OLD.status = 'trial_required'
+  AND NEW.status = 'trial_completed'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM trial_mix_records t
+    WHERE t.mix_design_id = NEW.id
+      AND t.batch_quantity_m3 > 0
+      AND t.actual_slump_mm >= 0
+      AND t.air_content_percent BETWEEN 0 AND 100
+      AND t.fresh_density_kg_m3 > 0
+  )
+BEGIN
+  SELECT RAISE(ABORT, 'trial_completed_requires_valid_trial_mix_record');
+END;
