1e6fd00bcec352b7a810ef11d9ebaeca4344a33e
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-04 20:59:51 +0330

    expose material library preload API

--- apps/desktop/electron/preload.ts
+++ apps/desktop/electron/preload.ts
@@ -35,6 +35,12 @@ contextBridge.exposeInMainWorld('tolouMaterials', {
   listByMixDesign: (mixDesignId: string) => ipcRenderer.invoke('materials:list-by-mix-design', mixDesignId)
 });
 
+contextBridge.exposeInMainWorld('tolouMaterialLibrary', {
+  save: (payload: unknown) => ipcRenderer.invoke('material-library:save', payload),
+  list: (materialType?: string) => ipcRenderer.invoke('material-library:list', materialType),
+  attach: (mixDesignId: string, libraryMaterialId: string) => ipcRenderer.invoke('material-library:attach', mixDesignId, libraryMaterialId)
+});
+
 contextBridge.exposeInMainWorld('tolouGradation', {
   save: (payload: unknown) => ipcRenderer.invoke('gradation:save', payload),
   listByMaterial: (materialId: string) => ipcRenderer.invoke('gradation:list-by-material', materialId)
