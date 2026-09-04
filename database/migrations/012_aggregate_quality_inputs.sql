1c8e6fb079d3ef12456f2161cdf3f86e254ec94e
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-04 12:35:48 +0330

    Add aggregate quality test fields

---
+++ database/migrations/012_aggregate_quality_inputs.sql
@@ -0,0 +1,7 @@
+ALTER TABLE materials ADD COLUMN astm_c117_finer_75um_percent REAL;
+ALTER TABLE materials ADD COLUMN finer_75um_limit_percent REAL;
+ALTER TABLE materials ADD COLUMN aggregate_test_evidence_ref TEXT;
+ALTER TABLE materials ADD COLUMN astm_c29_rodded_unit_weight_kg_m3 REAL;
+ALTER TABLE materials ADD COLUMN astm_c127_c128_ssd_specific_gravity REAL;
+ALTER TABLE materials ADD COLUMN astm_c127_c128_absorption_percent REAL;
+ALTER TABLE materials ADD COLUMN aggregate_quality_standard TEXT;
