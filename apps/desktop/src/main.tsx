7ff4fb0b8fd8d6aafdadcbc231ec46d81818c161
erfanamirij5-oss <erfanamirij5@gmail.com>
2026-09-04 23:29:02 +0330

    Route desktop through root module navigation

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { RootApp } from './RootApp';
 import './styles.css';
 import './forms.css';
 import './revision-control.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
